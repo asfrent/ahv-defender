2735bd2dad53e3f6cccbf4e8c0a269cfac6ade7b
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-12 21:26:37 +0100

    implemented thorough extractor

--- email-analyzer/email-analyzer.cc
+++ email-analyzer/email-analyzer.cc
@@ -137,6 +137,29 @@ class AHVExtractor {
   }
 
  protected:
+  static int CountDigitGroups(const std::string& s) {
+    // Find first digit.
+    size_t i = 0;
+    int count = 0;
+    while (i < s.size()) {
+      // Skip non digit sequence.
+      while (i < s.size() && !isdigit(s[i])) {
+        ++i;
+      }
+      // Did we hit the end?
+      if (i == s.size()) {
+        return count;
+      }
+      // Then we have a digit sequence.
+      ++count;
+      // Skip the digit sequence.
+      while (i < s.size() && isdigit(s[i])) {
+        ++i;
+      }
+    }
+    return count;
+  }
+
   // This method should be called each time a potential AHV is found at a higher
   // level. It converts the string to int64_t and checks its validity and, if
   // these checks pass, adds the converted AHV to the results.
@@ -157,24 +180,25 @@ class AHVExtractor {
 //   * Separators can only be one of ' ', '.', '-'.
 //   * The three digits in the beginning ('756') are never separated.
 //   * There is no more than one separator between two digits.
-//   * There's no more than 4 separators in total.
+//   * There's no more than 4 digit groups.
 class AHVExtractorStandard : public AHVExtractor {
  public:
   AHVExtractorStandard() {
-    regex_list_.push_back(std::regex(
-        "756[ \\.-]?([0-9][ \\.-]?){10}",
-        std::regex::optimize));
+    regex_list_.push_back(
+        std::regex( "756([ \\.\\-]?[0-9]){10}", std::regex::optimize));
   }
 
   void Process(const std::string& text) override {
+    const int max_digit_groups = 4;
+
     // Iterate over regex list.
     for (const std::regex re : regex_list_) {
       auto start = std::sregex_iterator(text.begin(), text.end(), re);
       auto stop = std::sregex_iterator();
       // Iterate over matches.
       for (std::sregex_iterator i = start; i != stop; ++i) {
-        // We filter out matches that have more than 4 separators.
-        if (CountSeparators(i->str()) > 4) {
+        // We filter out matches that have more digit groups than we allow.
+        if (CountDigitGroups(i->str()) > max_digit_groups) {
           continue;
         }
         // Report the match to the base class.
@@ -184,31 +208,61 @@ class AHVExtractorStandard : public AHVExtractor {
   }
 
  private:
-  static int CountSeparators(const std::string& ahv) {
-    int count = 0;
-    for (char c : ahv) {
-      if (!isdigit(c)) {
-        ++count;
-      }
-    }
-    return count;
-  }
-
   std::vector<std::regex> regex_list_;
 };
 
 // In THOROUGH mode we check additional AHV templates via regular
 // expressions, but we still look only for the saner of the possible forms:
 //   * More separators (eg. '_', '/', ',')
-//   * We allow separators to appear more than once between two digits,
+//   * We allow separators to appear at most twice between two digits,
 //     maybe with some additional restrictions.
-//   * We allow up to 6 separators.
+//   * We allow more digit groups than standard.
 class AHVExtractorThorough : public AHVExtractor {
  public:
-  void Process(const std::string& text) override {
-    // TODO implement.
+  AHVExtractorThorough() {
+    // We include the standard separators: ' ', '.', '-'
+    // We include some other (sane) separators: '/' '_' '\t'
+    // We allow for some typos: ',' '*' ':' ';'
+    // Note on typos: these are on keys that are close to standard separators or
+    // can be obtained by pressing shift on or aound standard separator keys.
+    // The list is not exhaustive and only takes into consideration German and US
+    // layouts.
+    std::string separators =
+        standard_separators_ + other_separators_ + typo_separators_;
+    regex_list_.push_back(
+        std::regex("756([" + separators + "]{0,2}[0-9]){10}", std::regex::optimize));
   }
+
+  void Process(const std::string& text) override {
+    const int max_digit_groups = 6;
+
+    // Iterate over regex list.
+    for (const std::regex re : regex_list_) {
+      auto start = std::sregex_iterator(text.begin(), text.end(), re);
+      auto stop = std::sregex_iterator();
+      // Iterate over matches.
+      for (std::sregex_iterator i = start; i != stop; ++i) {
+        // We filter out matches that have more digit groups than we allow.
+        if (CountDigitGroups(i->str()) > max_digit_groups) {
+          continue;
+        }
+        // Report the match to the base class.
+        Matched(i->str());
+      }
+    }
+  }
+
+ private:
+   static const std::string standard_separators_;
+   static const std::string other_separators_;
+   static const std::string typo_separators_;
+
+  std::vector<std::regex> regex_list_;
 };
+
+const std::string AHVExtractorThorough::standard_separators_ = " \\.\\-";
+const std::string AHVExtractorThorough::other_separators_ = "\\/_\\t";
+const std::string AHVExtractorThorough::typo_separators_ = ",\\*:;";
 
 // In PARANOID mode use a double ended queue to scan for any string that
 // contains 13 digits with only loose restrictions: they may be separated
@@ -242,12 +296,38 @@ std::string ReadAllFromStdin() {
   return sstream.str();
 }
 
+void PrintUsage() {
+  std::cerr << "Usage: ./email-analyzer standard|thorough|paranoid" << std::endl;
+}
+
+// Builds a new AHVExtractor from the supplied string specification.
+std::unique_ptr<AHVExtractor> NewExtractorFromSpec(const std::string& spec) {
+  if (spec == "standard") {
+    return std::make_unique<AHVExtractorStandard>();
+  } else if (spec == "thorough") {
+    return std::make_unique<AHVExtractorThorough>();
+  } else if (spec == "paranoid") {
+    return std::make_unique<AHVExtractorParanoid>();
+  } else {
+    PrintUsage();
+    exit(-1);
+  }
+}
+
 int main(int argc, char** argv) {
-  std::string text = ReadAllFromStdin();
-  std::unique_ptr<AHVExtractor> extractor(new AHVExtractorStandard);
-  extractor->Process(text);
+  // Check argument count.
+  if (argc < 2) {
+    PrintUsage();
+    exit(-1);
+  }
+
+  // Build extractor from spec and use it to process all text from standard
+  // input.
+  auto extractor = NewExtractorFromSpec(argv[1]);
+  extractor->Process(ReadAllFromStdin());
   for (int64_t ahv : extractor->Results()) {
     std::cout << ahv << std::endl;
   }
+
   return 0;
 }
