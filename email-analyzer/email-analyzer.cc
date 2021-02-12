0123969bc74654efb6f98639eaa60cfe8dafb74b
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-12 16:31:57 +0100

    simple lookup service, current performance (does nothing) is 2700 QPS from a single, serial client

--- email-analyzer/email-analyzer.cc
+++ email-analyzer/email-analyzer.cc
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <memory>
+#include <string>
+#include <chrono>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello!" << std::endl;
+#include <grpcpp/grpcpp.h>
+
+#include "ahvdefender.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using ahvdefender::AHVDatabase;
+using ahvdefender::AHVLookupRequest;
+using ahvdefender::AHVLookupResponse;
+
+class AHVDatabaseClient {
+ public:
+  AHVDatabaseClient(std::shared_ptr<Channel> channel)
+      : stub_(AHVDatabase::NewStub(channel)) {}
+
+  bool Lookup(int64_t ahv) {
+    AHVLookupRequest request;
+    request.set_ahv(ahv);
+
+    AHVLookupResponse response;
+
+    ClientContext context;
+    Status status = stub_->Lookup(&context, request, &response);
+
+    if (status.ok()) {
+      return response.found();
+    } else {
+      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+      // TODO we should probably create a strict mode which would allow us to
+      // control the returned value in case of failure.
+      return false;
+    }
+  }
+
+ private:
+  std::unique_ptr<AHVDatabase::Stub> stub_;
+};
+
+int main(int argc, char** argv) {
+  auto insecure_credentials = grpc::InsecureChannelCredentials();
+  auto grpc_channel = grpc::CreateChannel("localhost:12000", insecure_credentials);
+  AHVDatabaseClient client(grpc_channel);
+  auto start_time = std::chrono::high_resolution_clock::now();
+
+  const int iterations = 10000;
+  for (int i = 0; i < iterations; ++i) {
+    client.Lookup(123);
+  }
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  std::cout << "Took " << duration.count() << "ms." << std::endl;
+
+  int qps = (int) (iterations * 1000 / duration.count());
+  std::cout << "QPS: " << qps << std::endl;
+
   return 0;
 }
--- lookup-server/lookup-server.cc
+++ lookup-server/lookup-server.cc
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Hello!" << std::endl;
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+
+#include "ahvdefender.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+using ahvdefender::AHVDatabase;
+using ahvdefender::AHVLookupRequest;
+using ahvdefender::AHVLookupResponse;
+
+class AHVDatabaseServiceImpl final : public AHVDatabase::Service {
+  Status Lookup(ServerContext* context, const AHVLookupRequest* request, AHVLookupResponse* response) override {
+    response->set_found(true);
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string server_address("0.0.0.0:12000");
+  AHVDatabaseServiceImpl service;
+  grpc::EnableDefaultHealthCheckService(true);
+  grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  RunServer();
   return 0;
 }
