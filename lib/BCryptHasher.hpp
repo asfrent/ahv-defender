4b51f3a84f82a424ccbc585726f8d8a8c00fe9dc
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-13 21:11:40 +0100

    Extracted lib/BCryptHasher.hpp.

---
+++ lib/BCryptHasher.hpp
@@ -0,0 +1,40 @@
+#ifndef AHV_DEFENDER_BCRYPT_HASHER_H_
+#define AHV_DEFENDER_BCRYPT_HASHER_H_
+
+#define BCRYPT_INPUT_LEN 16
+#define BCRYPT_HASH_LEN 64
+#define BCRYPT_FACTOR 8
+
+extern "C" {
+char *crypt_gensalt_rn(__const char *prefix, unsigned long count, __const char *input, int size, char *output, int output_size);
+char *crypt_rn(__const char *key, __const char *setting, void *data, int size);
+}
+
+class BCryptHasher {
+ public:
+  BCryptHasher() {
+    memset(setting, 0, BCRYPT_HASH_LEN);
+    crypt_gensalt_rn("$2a$", BCRYPT_FACTOR, (char*) input, BCRYPT_INPUT_LEN, setting, BCRYPT_HASH_LEN);
+    std::cout << "Initialized bcrypt setting: " << setting << std::endl;
+  }
+
+  std::string ComputeHash(const std::string& plaintext) {
+    char hash[BCRYPT_HASH_LEN] = {0};
+    crypt_rn(plaintext.c_str(), setting, hash, BCRYPT_HASH_LEN);
+    std::string result(hash);
+    result = result.substr(result.size() - 31);
+    std::cout << plaintext << " --> " << result << std::endl;
+    return result;
+  }
+
+ private:
+  char setting[BCRYPT_HASH_LEN];
+  static const unsigned char input[BCRYPT_INPUT_LEN];
+};
+
+const unsigned char BCryptHasher::input[16] = {
+  0xfb, 0xf5, 0x7f, 0xb4, 0x30, 0xe7, 0x07, 0x10,
+  0xa3, 0x1a, 0x8a, 0x03, 0x06, 0x26, 0xd0, 0xcb,
+};
+
+#endif  // AHV_DEFENDER_BCRYPT_HASHER_H_
--- lookup-server/lookup-server.cc
+++ lookup-server/lookup-server.cc
@@ -17,6 +17,8 @@
 
 #include "ahvdefender.grpc.pb.h"
 
+#include "BCryptHasher.hpp"
+
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
@@ -44,42 +46,6 @@ void SetUpSigIntHandler() {
   sig_int_handler.sa_flags = 0;
   sigaction(SIGINT, &sig_int_handler, nullptr);
 }
-
-#define BCRYPT_INPUT_LEN 16
-#define BCRYPT_HASH_LEN 64
-#define BCRYPT_FACTOR 8
-
-extern "C" {
-char *crypt_gensalt_rn(__const char *prefix, unsigned long count, __const char *input, int size, char *output, int output_size);
-char *crypt_rn(__const char *key, __const char *setting, void *data, int size);
-}
-
-class BCryptHasher {
- public:
-  BCryptHasher() {
-    memset(setting, 0, BCRYPT_HASH_LEN);
-    crypt_gensalt_rn("$2a$", BCRYPT_FACTOR, (char*) input, BCRYPT_INPUT_LEN, setting, BCRYPT_HASH_LEN);
-    std::cout << "Initialized bcrypt setting: " << setting << std::endl;
-  }
-
-  std::string ComputeHash(const std::string& plaintext) {
-    char hash[BCRYPT_HASH_LEN] = {0};
-    crypt_rn(plaintext.c_str(), setting, hash, BCRYPT_HASH_LEN);
-    std::string result(hash);
-    result = result.substr(result.size() - 31);
-    std::cout << plaintext << " --> " << result << std::endl;
-    return result;
-  }
-
- private:
-  char setting[BCRYPT_HASH_LEN];
-  static const unsigned char input[BCRYPT_INPUT_LEN];
-};
-
-const unsigned char BCryptHasher::input[16] = {
-  0xfb, 0xf5, 0x7f, 0xb4, 0x30, 0xe7, 0x07, 0x10,
-  0xa3, 0x1a, 0x8a, 0x03, 0x06, 0x26, 0xd0, 0xcb,
-};
 
 struct DiskRecord {
   void set_used(bool used) {
