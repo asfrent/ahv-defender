af469e36d8fcae0d21d684518749e72207ac567d
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-13 20:48:29 +0100

    Extracted lib/AHVDatabaseClient.hpp / reorgaized CMakeLists.txt

---
+++ lib/AHVDatabaseClient.hpp
@@ -0,0 +1,74 @@
+#ifndef AHV_DEFENDER_AHV_DATABASE_CLIENT_H_
+#define AHV_DEFENDER_AHV_DATABASE_CLIENT_H_
+
+#include <grpcpp/grpcpp.h>
+
+#include "ahvdefender.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using ahvdefender::AHVDatabase;
+using ahvdefender::AHVLookupRequest;
+using ahvdefender::AHVLookupResponse;
+using ahvdefender::AHVAddRequest;
+using ahvdefender::AHVAddResponse;
+using ahvdefender::AHVRemoveRequest;
+using ahvdefender::AHVRemoveResponse;
+
+class AHVDatabaseClient {
+ public:
+  bool Lookup(const std::string& ahv) {
+    AHVLookupRequest request;
+    request.set_ahv(ahv);
+    AHVLookupResponse response;
+    ClientContext context;
+    Status status = stub_->Lookup(&context, request, &response);
+    if (!status.ok()) {
+      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+      exit(1);
+    }
+    return response.found();
+  }
+
+  bool Add(const std::string& ahv) {
+    AHVAddRequest request;
+    request.set_ahv(ahv);
+    AHVAddResponse response;
+    ClientContext context;
+    Status status = stub_->Add(&context, request, &response);
+    if (!status.ok()) {
+      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+      exit(1);
+    }
+    return response.added();
+  }
+
+  bool Remove(const std::string& ahv) {
+    AHVRemoveRequest request;
+    request.set_ahv(ahv);
+    AHVRemoveResponse response;
+    ClientContext context;
+    Status status = stub_->Remove(&context, request, &response);
+    if (!status.ok()) {
+      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
+      exit(1);
+    }
+    return response.removed();
+  }
+
+  static std::unique_ptr<AHVDatabaseClient> New(const std::string& target) {
+    auto insecure_credentials = grpc::InsecureChannelCredentials();
+    auto grpc_channel = grpc::CreateChannel(target, insecure_credentials);
+    return std::unique_ptr<AHVDatabaseClient>(new AHVDatabaseClient(grpc_channel));
+  }
+
+ private:
+  AHVDatabaseClient(std::shared_ptr<Channel> channel)
+      : stub_(AHVDatabase::NewStub(channel)) {}
+
+  std::unique_ptr<AHVDatabase::Stub> stub_;
+};
+
+#endif  // AHV_DEFENDER_AHV_DATABASE_CLIENT_H_
--- lookup-server/cli.cc
+++ lookup-server/cli.cc
@@ -1,82 +1,9 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include <chrono>
-#include <cctype>
-#include <sstream>
-#include <regex>
-#include <deque>
 #include <functional>
 
-#include <grpcpp/grpcpp.h>
-
-#include "ahvdefender.grpc.pb.h"
-
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-
-using ahvdefender::AHVDatabase;
-using ahvdefender::AHVLookupRequest;
-using ahvdefender::AHVLookupResponse;
-using ahvdefender::AHVAddRequest;
-using ahvdefender::AHVAddResponse;
-using ahvdefender::AHVRemoveRequest;
-using ahvdefender::AHVRemoveResponse;
-
-class AHVDatabaseClient {
- public:
-  bool Lookup(const std::string& ahv) {
-    AHVLookupRequest request;
-    request.set_ahv(ahv);
-    AHVLookupResponse response;
-    ClientContext context;
-    Status status = stub_->Lookup(&context, request, &response);
-    if (!status.ok()) {
-      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
-      exit(1);
-    }
-    return response.found();
-  }
-
-  bool Add(const std::string& ahv) {
-    AHVAddRequest request;
-    request.set_ahv(ahv);
-    AHVAddResponse response;
-    ClientContext context;
-    Status status = stub_->Add(&context, request, &response);
-    if (!status.ok()) {
-      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
-      exit(1);
-    }
-    return response.added();
-  }
-
-  bool Remove(const std::string& ahv) {
-    AHVRemoveRequest request;
-    request.set_ahv(ahv);
-    AHVRemoveResponse response;
-    ClientContext context;
-    Status status = stub_->Remove(&context, request, &response);
-    if (!status.ok()) {
-      std::cerr << status.error_code() << ": " << status.error_message() << std::endl;
-      exit(1);
-    }
-    return response.removed();
-  }
-
-  static std::unique_ptr<AHVDatabaseClient> New(const std::string& target) {
-    auto insecure_credentials = grpc::InsecureChannelCredentials();
-    auto grpc_channel = grpc::CreateChannel(target, insecure_credentials);
-    return std::unique_ptr<AHVDatabaseClient>(new AHVDatabaseClient(grpc_channel));
-  }
-
- private:
-  AHVDatabaseClient(std::shared_ptr<Channel> channel)
-      : stub_(AHVDatabase::NewStub(channel)) {}
-
-  std::unique_ptr<AHVDatabase::Stub> stub_;
-};
+#include "AHVDatabaseClient.hpp"
 
 void PrintUsage() {
   std::cerr << "Usage: ./cli db_server_address add|remove|lookup [quiet]" << std::endl;
@@ -139,6 +66,7 @@ int main(int argc, char** argv) {
   }
 
   // Read all lines, execute action.
+  std::ios::sync_with_stdio(false);
   std::string ahv;
   while (std::getline(std::cin, ahv)) {
     f(ahv);
