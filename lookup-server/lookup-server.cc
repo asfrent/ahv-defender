631175b6d94f94fea80f53d939b2d4747f1907bc
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-13 03:35:36 +0100

    hashing w/ bcrypt from stdin

--- lookup-server/lookup-server.cc
+++ lookup-server/lookup-server.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <cstdlib>
 
 #include <grpcpp/grpcpp.h>
 #include <grpcpp/health_check_service_interface.h>
@@ -16,6 +17,39 @@ using grpc::Status;
 using ahvdefender::AHVDatabase;
 using ahvdefender::AHVLookupRequest;
 using ahvdefender::AHVLookupResponse;
+
+#define BCRYPT_INPUT_LEN 16
+#define BCRYPT_HASH_LEN 64
+#define BCRYPT_FACTOR 13
+
+extern "C" {
+char *crypt_gensalt_rn(__const char *prefix, unsigned long count, __const char *input, int size, char *output, int output_size);
+char *crypt_rn(__const char *key, __const char *setting, void *data, int size);
+}
+
+class BCryptHasher {
+ public:
+  BCryptHasher() {
+    memset(setting, 0, BCRYPT_HASH_LEN);
+    crypt_gensalt_rn("$2a$", BCRYPT_FACTOR, (char*) input, BCRYPT_INPUT_LEN, setting, BCRYPT_HASH_LEN);
+    std::cout << "Initialized bcrypt setting: " << setting << std::endl;
+  }
+
+  std::string ComputeHash(const std::string& plaintext) {
+    char hash[BCRYPT_HASH_LEN] = {0};
+    crypt_rn(plaintext.c_str(), setting, hash, BCRYPT_HASH_LEN);
+    return std::string(hash);
+  }
+
+ private:
+  char setting[BCRYPT_HASH_LEN];
+  static const unsigned char input[BCRYPT_INPUT_LEN];
+};
+
+const unsigned char BCryptHasher::input[16] = {
+  0xfb, 0xf5, 0x7f, 0xb4, 0x30, 0xe7, 0x07, 0x10,
+  0xa3, 0x1a, 0x8a, 0x03, 0x06, 0x26, 0xd0, 0xcb,
+};
 
 class AHVDatabaseServiceImpl final : public AHVDatabase::Service {
   Status Lookup(ServerContext* context, const AHVLookupRequest* request, AHVLookupResponse* response) override {
@@ -39,6 +73,9 @@ void RunServer() {
 }
 
 int main(int argc, char** argv) {
-  RunServer();
+  auto hasher = std::make_unique<BCryptHasher>();
+  std::string plaintext;
+  std::getline(std::cin, plaintext);
+  std::cout << hasher->ComputeHash(plaintext) << std::endl;
   return 0;
 }
