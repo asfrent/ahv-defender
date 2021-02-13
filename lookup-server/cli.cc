c4270cc296431bba934bcdc8dea8bdaa68bbf292
Andrei Sfrent <andreisfrent@gmail.com>
2021-02-13 20:17:55 +0100

    read multimple ahvs from stdin. also added quiet mode

--- lookup-server/cli.cc
+++ lookup-server/cli.cc
@@ -79,30 +79,69 @@ class AHVDatabaseClient {
 };
 
 void PrintUsage() {
-  std::cerr << "Usage: ./cli db_server_address add|remove|lookup ahv" << std::endl;
+  std::cerr << "Usage: ./cli db_server_address add|remove|lookup [quiet]" << std::endl;
 }
 
 int main(int argc, char** argv) {
   // Check argument count.
-  if (argc != 4) {
+  if (argc < 3 || argc > 4) {
     PrintUsage();
     exit(1);
   }
 
-  auto ahv_database_client = AHVDatabaseClient::New(argv[1]);
+  // Find target, action from args.
+  std::string target(argv[1]);
   std::string action(argv[2]);
-  std::string ahv(argv[3]);
 
+  // Sort out quiet arg.
+  bool quiet = false;
+  if (argc == 4) {
+    if (strcmp(argv[3], "quiet") == 0) {
+      quiet = true;
+    } else {
+      PrintUsage();
+      exit(1);
+    }
+  }
+
+  // Init db client.
+  auto ahv_database_client = AHVDatabaseClient::New(target);
+
+  // One lambda per action.
+  auto add_fn = [&] (const std::string& ahv) -> void {
+    bool result = ahv_database_client->Add(ahv);
+    if (quiet) return;
+    std::cout << (result ? "true" : "false") << std::endl;
+  };
+  auto remove_fn = [&] (const std::string& ahv) -> void {
+    bool result = ahv_database_client->Remove(ahv);
+    if (quiet) return;
+    std::cout << (result ? "true" : "false") << std::endl;
+  };
+  auto lookup_fn = [&] (const std::string& ahv) -> void {
+    bool result = ahv_database_client->Lookup(ahv);
+    if (quiet) return;
+    std::cout << (result ? "true" : "false") << std::endl;
+  };
+
+  // Choose the lambda based on action arg.
+  std::function<void(const std::string& ahv)> f;
   if (action == "add") {
-    std::cout << (ahv_database_client->Add(ahv) ? "true" : "false") << std::endl;
+    f = add_fn;
   } else if (action == "remove") {
-    std::cout << (ahv_database_client->Remove(ahv) ? "true" : "false") << std::endl;
+    f = remove_fn;
   } else if (action == "lookup") {
-    std::cout << (ahv_database_client->Lookup(ahv) ? "true" : "false") << std::endl;
+    f = lookup_fn;
   } else {
     std::cerr << "Unknown action \"" << action << "\"." << std::endl;
     PrintUsage();
     exit(1);
+  }
+
+  // Read all lines, execute action.
+  std::string ahv;
+  while (std::getline(std::cin, ahv)) {
+    f(ahv);
   }
 
   return 0;
